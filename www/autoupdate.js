846deb1fba3ec593b6e982b771954e9c63f2f132
石头 <258137678@qq.com>
2015-12-21 18:07:11 +0800

    添加更新逻辑

--- www/autoupdate.js
+++ www/autoupdate.js
@@ -3,92 +3,61 @@
  * email:258137678@qq.com
  */
 (function () {
-    function loadIndexHtml() {
-        window.pegasus(Manifest.root + '_index.html').then(function (template) {
-            $('body').html(template);
-            require(['components/page/app']);
 
-        }, function (xhr) {
-            console.error('Could not download ' + url + ': ' + xhr.status);
-        })
+    var Updata = function () {
+        // Check for Cordova
+        var SERVER = 'http://192.168.199.64:8089/cordova-seed/www/';
 
-    }
-    loadIndexHtml()
+        var fs = new CordovaPromiseFS({persistent: typeof cordova !== 'undefined'});
 
-    // Check for Cordova
-    var isCordova = typeof cordova !== 'undefined',
-    // CordovaPromiseFS
-        fs,
-    // CordovaFileLoader
-        loader,
-    // script-tag...
-        script,
-    // ...that contains the serverRoot
-        serverRoot;
+        var loader = window.loader = new CordovaAppLoader({
+            fs: fs,
+            localRoot: 'app',
+            serverRoot: SERVER,
+            mode: 'mirror',
+            cacheBuster: true
+        });
 
-    // Get serverRoot from script tag.
-    script = document.querySelector('script[server]');
-    if (script) serverRoot = script.getAttribute('server');
-    if (!serverRoot) {
-        throw new Error('Add a "server" attribute to the bootstrap.js script!');
-    }
 
-    // Initialize filesystem and loader
-    fs = new CordovaPromiseFS({
-        persistent: isCordova, // Chrome should use temporary storage.
-        Promise: Promise
-    });
 
-    loader = new CordovaAppLoader({
-        fs: fs,
-        localRoot: 'app',
-        serverRoot: serverRoot,
-        mode: 'mirror',
-        cacheBuster: true
-    });
-
-    function onProgress(ev) {
-        $("#loadinfo").html((ev.percentage * 100) + '%')
-        console.log((ev.percentage * 100) + '%');
-    }
-
-    // Check > Download > Update
-    function check() {
-        loader.check(function () {
-
-        })
-            .then(function () {
-
-                return loader.download(onProgress);
+        function loadIndexHtml() {
+            window.pegasus(Manifest.root + '_index.html').then(function (template) {
+                $('body').html(template);
+                require(['components/page/app']);
+            }, function (xhr) {
+                console.error('Could not download ' + url + ': ' + xhr.status);
             })
-            .then(function () {
-                return loader.update();
-            }, function (err) {
-                console.error('Auto-update error:', err);
-            });
+        }
+
+        return {
+            updateAvailable: false,
+            init: function () {
+                loadIndexHtml()
+            },
+            check: function (success, error) {
+                var me = this;
+                loader.check().then(function (updateAvailable) {
+                    me.updateAvailable = updateAvailable;
+                    success();
+                }, error);
+            },
+            download: function (onProgress,success, error) {
+                loader.download(onProgress).then(success, error);
+            },
+            update: function () {
+                loader.update();
+            }
+        }
+
     }
 
-    // Couple events:
-
-    // 1. On launch
-    check();
-
+    var updataMg = window.UpdateMg = new Updata();
+    updataMg.init();
     // 2. Cordova: On resume
-    fs.deviceready.then(function () {
-        document.addEventListener('resume', check);
+    document.addEventListener('resume', function () {
+        UpdateMg.check()
     });
 
-    // 3. Chrome: On page becomes visible again
-    function handleVisibilityChange() {
-        if (!document.webkitHidden) {
-            check();
-        }
-    }
-
-    document.addEventListener("webkitvisibilitychange", handleVisibilityChange, false);
     window.BOOTSTRAP_OK = true;
-
-
-
 
 })();
